8bcaa69c875f56bc65c62d8c179ec599837f874f
Wayan Jimmy <jimmyeatcrab@gmail.com>
2019-02-06 17:54:14 +0800

    test lint-staged

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 
 const CopyNotification = ({ showNotif, message }) => (
-  <div className={`notification is-success ${showNotif ? 'notif__up' : ''}`}>
-  {message}
-  </div>
-)
+  <div className={`notification is-success ${showNotif ? 'notif__up' : ''}`}>{message}</div>
+);
 
+const AlertNotification = ({ showNotif, message }) => (
+  <div className={`notification is-danger ${showNotif ? 'notif__up' : ''}`}>{message}</div>
+);
 
-const AlertNotification = ({showNotif, message}) => (
-  <div className={`notification is-danger ${showNotif ? 'notif__up' : ''}`}>    
-  {message}
-  </div>
-)
-
-export {
-  CopyNotification,
-  AlertNotification
-};
+export { CopyNotification, AlertNotification };
