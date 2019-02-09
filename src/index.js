4af6c7c26a3514f30f92ab634ce3b03bb5a47ecc
imdbsd <budisuryadarma0407@gmail.com>
2019-02-09 16:16:16 +0800

    adding ga

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './main.css';
 import Page404 from './pages/404';
 import Home from './pages/Home';
@@ -8,23 +8,34 @@ import Footer from './styles/Footer';
 import EmojiDetails from './pages/EmojiDetails';
 import * as serviceWorker from './serviceWorker';
 
-class Index extends React.Component{  
-  render(){
-    console.log({props: this.props})
-    return(
-      <Router>
-        <React.Fragment>          
+class Index extends React.Component {
+  render() {
+    console.log({ props: this.props });
+    return (
+      <Router basename="/kaomoji">
+        <React.Fragment>
           <Switch>
-            <Route path="/" exact component={Home} />  
-            <Route path="/:filter/:parentCategory/:subCategory/:index" component={EmojiDetails} />
+            <Route path="/" exact component={Home} />
+            <Route
+              path="/:filter/:parentCategory/:subCategory/:index"
+              component={EmojiDetails}
+            />
             <Route component={Page404} />
           </Switch>
           <Footer>
-            Develop with &nbsp;<span>&#10084;</span>&nbsp; by &nbsp;<a href="https://twitter.com/Budisuryadarma" target="_blank" rel="noopener noreferrer" title="imdbsd twitter link">imdbsd</a>
+            Develop with &nbsp;<span>&#10084;</span>&nbsp; by &nbsp;
+            <a
+              href="https://twitter.com/Budisuryadarma"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="imdbsd twitter link"
+            >
+              imdbsd
+            </a>
           </Footer>
-        </React.Fragment>        
+        </React.Fragment>
       </Router>
-    )
+    );
   }
 }
 
